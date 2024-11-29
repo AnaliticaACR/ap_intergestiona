ef73e2354155866e7a643300b72cb3d8781a3338
William Castro <willcr32@gmail.com>
2024-11-29 16:59:54 -0500

    cargar datos2

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-import streamlit as st
-import pandas as pd
 import requests
+import pandas as pd
+import streamlit as st
 
 @st.cache_data
 def cargar_datos():
-    try:
+    try:https://github.com/williamCastro32/app_intergestion/blob/main/app_datos/gestiones_interg-parquet
         # URL raw del archivo en GitHub
-        url = "https://raw.githubusercontent.com/williamCastro32/Coor_datos/main/app_datos/gestiones_interg-parquet"
+        url = "https://raw.githubusercontent.com/williamCastro32/app_intergestion/main/app_datos/gestiones_interg-parquet"
         
-        # Descargar el contenido
+        # Descargar el contenido del archivo
         response = requests.get(url)
         response.raise_for_status()  # Lanzará un error si la descarga falla
         
-        # Cargar desde bytes
-        return pd.read_parquet(io.BytesIO(response.content))
+        # Guardar el archivo temporalmente
+        with open("/tmp/gestiones_interg-parquet", "wb") as f:
+            f.write(response.content)
+        
+        # Cargar el archivo Parquet desde el archivo temporal
+        return pd.read_parquet("/tmp/gestiones_interg-parquet")
     
     except Exception as e:
         st.error(f"Error al cargar los datos: {e}")
         return pd.DataFrame()  # Devuelve un DataFrame vacío en caso de error
+
 
 # Resto del código igual
 # 1. Título de la aplicación
